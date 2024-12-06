e59af182385e14938db7ebe1329f7f92b03f9e59
RBaykan <dmental123@gmail.com>
2024-12-06 18:18:28 +0300

    Update MailConfiguration.java

--- java/com/nontius/proje/configuration/MailConfiguration.java
+++ java/com/nontius/proje/configuration/MailConfiguration.java
@@ -10,23 +10,5 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 @Configuration
 public class MailConfiguration {
 	
-	/*
-	@Bean
-	public JavaMailSender javaMailSender() {
-		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-		
-		mailSender.setHost("smtp.elasticemail.com");
-		mailSender.setPort(2525);
-		
-		
-		mailSender.setUsername("deneme@deneme.com");
-		mailSender.setPassword("0FC10F8A5F3A51E626E14D4E64441B006056");
-		
-		
-
-        return mailSender;
-		
-		
-	}*/
-
+	
 }
